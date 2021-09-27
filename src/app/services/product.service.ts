f0bd8ba76367772fe874d19c725af54894ae86bb
ldex <laurent.duveau+git@gmail.com>
2021-09-27 16:25:07 -0400

    pagination

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, delay, shareReplay, tap, map } from 'rxjs/operators';
+import { Product } from '../products/product.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  private baseUrl = 'https://storerestservice.azurewebsites.net/api/products/';
+  products$: Observable<Product[]>;
+
+  constructor(private http: HttpClient) {
+    this.initProducts();
+  }
+
+  initProducts() {
+    this.products$ = this
+                        .http
+                        .get<Product[]>(this.baseUrl)
+                        .pipe(
+                          delay(1500), // fake delay!!
+                          tap(console.table)
+                        );
+  }
+}
